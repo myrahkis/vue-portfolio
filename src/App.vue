5996454dd6e6bbf5ccaca38122809c2d6e858859
myrahkis <kruzinka@yandex.ru>
2025-07-23 21:50:11 +0300

    setting up project

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { RouterView } from "vue-router";
+</script>
+
+<template>
+  <h1>AAAAAAAAAAAAAA</h1>
+  <RouterView />
+  <h3>HEHEHE</h3>
+</template>
+
+<style scoped></style>
---
+++ src/views/home.vue
@@ -0,0 +1,5 @@
+<script setup></script>
+
+<template>
+  <h1 style="font-size: 5rem; color: red">AAAAAAAAAAAAAA</h1>
+</template>
